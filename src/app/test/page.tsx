7167a8a26652f26b6956f0c23c0a687aef48cb46
Romain Paris <romainparis@MacBook-Pro-de-Romain-2.local>
2025-10-16 18:18:28 -0400

    first commit

---
+++ src/app/test/page.tsx
@@ -0,0 +1,3 @@
+export default function TEST() {
+  return <div className="page">TEST</div>;
+}
---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "@/server/api/trpc";
+import { TRPCError } from "@trpc/server";
+
+export const postRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  create: protectedProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      if (!ctx.user) {
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+      }
+      return ctx.db.post.create({
+        data: {
+          name: input.name,
+          userId: ctx.user.id,
+        },
+      });
+    }),
+
+  getLatest: publicProcedure.query(async ({ ctx }) => {
+    const post = await ctx.db.post.findFirst({
+      orderBy: { createdAt: "desc" },
+    });
+
+    return post ?? null;
+  }),
+});
